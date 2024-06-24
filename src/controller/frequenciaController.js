537b89d4e968e3443ba8fc7df9f7a67547c4e30c
Vinícius De Oliveira Santana <vini.oli.santana@gmail.com>
2024-06-23 23:15:50 -0300

    frequencia funcionando

--- src/controller/frequenciaController.js
+++ src/controller/frequenciaController.js
@@ -1,182 +1,100 @@
 const database = require('../models');
 const { Op } = require('sequelize');
+const { format } = require('date-fns'); // Importar a função format do date-fns
 
-/**
- * @swagger
- * tags:
- *   name: Frequencia
- *   description: Endpoints para gerenciar frequências.
- */
-
-/**
- * @swagger
- * /frequencias:
- *   get:
- *     summary: Retorna todas as frequências.
- *     tags: [Frequencia]
- *     description: Endpoint para buscar todas as frequências.
- *     responses:
- *       200:
- *         description: Array de objetos frequência.
- *       500:
- *         description: Erro ao buscar frequências.
- */
 async function findAll(req, res) {
     try {
-        const frequencias = await database.Frequencia.findAll();
-        res.status(200).json(frequencias);
-    } catch (err) {
-        res.status(500).json({ mensagem: 'Erro ao buscar frequências', erro: err.message });
+        const frequenciaArray = await database.frequencia.findAll({
+            include: {
+                model: database.pacientes,
+                as: 'paciente',
+                on: {
+                    '$frequencia.pacienteId$': { [Op.col]: 'paciente.id' }
+                }
+            }
+        });
+
+        // Formatar a data de createdAt e updatedAt
+        const formattedFrequenciaArray = frequenciaArray.map(frequencia => {
+            return {
+                id: frequencia.id,
+                pacienteId: frequencia.pacienteId,
+                createdAt: format(new Date(frequencia.createdAt), 'dd/MM/yyyy HH:mm:ss'),
+                updatedAt: format(new Date(frequencia.updatedAt), 'dd/MM/yyyy HH:mm:ss'),
+                paciente: frequencia.paciente // Manter o objeto paciente como está
+            };
+        });
+
+        res.status(200).json(formattedFrequenciaArray);
+    } catch(err) {
+        res.status(500).json({ mensagem: 'Erro ao buscar frequencias', erro: err.message });
     }
 }
 
-/**
- * @swagger
- * /frequencias/{id}:
- *   get:
- *     summary: Retorna frequências de um paciente pelo ID.
- *     tags: [Frequencia]
- *     description: Endpoint para buscar frequências de um paciente pelo ID.
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: integer
- *         description: ID do paciente para buscar frequências.
- *     responses:
- *       200:
- *         description: Objeto frequência encontrado.
- *       500:
- *         description: Erro ao buscar paciente.
- */
 async function findById(req, res) {
-    const { id } = req.params;
     try {
-        const frequencias = await database.Frequencia.findAll({
-            where: { pacienteId: id },
-        });
-        res.status(200).json(frequencias);
-    } catch (err) {
-        res.status(500).json({ mensagem: 'Erro ao buscar frequências', erro: err.message });
+        const paciente = await database.frequencia.findAll(
+            {   
+                include: {
+                    model: database.pacientes,
+                    as: 'paciente',
+                    on: {
+                        '$frequencia.pacienteId$': { [Op.col]: 'paciente.id' }
+                    }
+                },
+                where : {
+                    pacienteId: req.params.id
+                }
+            }
+        );
+
+        // Formatar a data de createdAt e updatedAt
+        const formattedPaciente = paciente.map(item => ({
+            id: item.id,
+            pacienteId: item.pacienteId,
+            createdAt: format(new Date(item.createdAt), 'dd/MM/yyyy HH:mm:ss'),
+            updatedAt: format(new Date(item.updatedAt), 'dd/MM/yyyy HH:mm:ss'),
+            paciente: item.paciente // Manter o objeto paciente como está
+        }));
+
+        res.status(200).json(formattedPaciente);
+    } catch(err) {
+        res.status(500).json({ mensagem: 'Erro ao buscar paciente', erro: err.message });
     }
 }
 
-/**
- * @swagger
- * /frequencias:
- *   post:
- *     summary: Cria uma nova frequência.
- *     tags: [Frequencia]
- *     description: Endpoint para criar uma nova frequência.
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               pacienteId:
- *                 type: integer
- *             example:
- *               pacienteId: 1
- *     responses:
- *       200:
- *         description: Objeto frequência criada.
- *       500:
- *         description: Erro ao criar frequência.
- */
 async function create(req, res) {
-    const { pacienteId } = req.body;
     try {
-        const frequencia = await database.Frequencia.create({
-            pacienteId,
+        // Encontrar o paciente pelo CPF fornecido
+        const paciente = await database.pacientes.findOne({
+            where: {
+                cpf: req.body.cpf // Supondo que o CPF seja enviado no corpo da requisição
+            }
         });
+
+        if (!paciente) {
+            return res.status(404).json({ mensagem: 'Paciente não encontrado com o CPF fornecido' });
+        }
+
+        // Criar a frequência associada ao paciente encontrado
+        const frequencia = await database.frequencia.create({
+            pacienteId: paciente.id
+        });
+
         res.status(200).json(frequencia);
-    } catch (err) {
-        res.status(500).json({ mensagem: 'Erro ao criar frequência', erro: err.message });
+    } catch(err) {
+        res.status(500).json({ mensagem: 'Erro ao criar frequencia', erro: err.message });
     }
 }
 
-/**
- * @swagger
- * /frequencias/{id}:
- *   put:
- *     summary: Altera uma frequência pelo ID.
- *     tags: [Frequencia]
- *     description: Endpoint para alterar uma frequência pelo ID.
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: integer
- *         description: ID da frequência a ser alterada.
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               pacienteId:
- *                 type: integer
- *             example:
- *               pacienteId: 2
- *     responses:
- *       200:
- *         description: Objeto frequência alterada.
- *       500:
- *         description: Erro ao buscar frequência.
- */
-async function alter(req, res) {
-    const { id } = req.params;
-    const { pacienteId } = req.body;
-    try {
-        const frequencia = await database.Frequencia.findByPk(id);
-        if (!frequencia) {
-            res.status(404).json({ mensagem: 'Frequência não encontrada' });
-        } else {
-            await frequencia.update({ pacienteId });
-            res.status(200).json(frequencia);
-        }
-    } catch (err) {
-        res.status(500).json({ mensagem: 'Erro ao buscar frequência', erro: err.message });
-    }
-}
-
-/**
- * @swagger
- * /frequencias/{id}:
- *   delete:
- *     summary: Remove uma frequência pelo ID.
- *     tags: [Frequencia]
- *     description: Endpoint para remover uma frequência pelo ID.
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: integer
- *         description: ID da frequência a ser removida.
- *     responses:
- *       200:
- *         description: Mensagem de sucesso.
- *       500:
- *         description: Erro ao buscar frequência.
- */
 async function remove(req, res) {
-    const { id } = req.params;
     try {
-        const frequencia = await database.Frequencia.findByPk(id);
-        if (!frequencia) {
-            res.status(404).json({ mensagem: 'Frequência não encontrada' });
-        } else {
-            await frequencia.destroy();
-            res.status(200).json({ mensagem: 'Frequência excluída com sucesso', ok: true });
-        }
-    } catch (err) {
-        res.status(500).json({ mensagem: 'Erro ao buscar frequência', erro: err.message });
+        const frequencia = await database.frequencia.findByPk(req.params.id);
+
+        frequencia.destroy();  
+        res.status(200).json({ mensagem: `Frequencia excluída com sucesso`, ok: true }); 
+    } catch(err) {
+        res.status(500).json({ mensagem: 'Erro ao buscar frequencia', erro: err.message });
     }
 }
 
@@ -184,6 +102,5 @@ module.exports = {
     findAll,
     findById,
     create,
-    alter,
-    remove,
+    remove
 };
--- src/routes/frequenciaRoutes.js
+++ src/routes/frequenciaRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controller/frequenciaController');
+const { eAdmin } = require('../middlewares/auth');
 
-router.get('/', controller.findAll);
+router.get('/', eAdmin, controller.findAll);
 
-router.get('/:id', controller.findById);
+router.get('/:id', eAdmin, controller.findById);
 
-router.post('/', controller.create);
+router.post('/', eAdmin, controller.create);
 
-router.delete('/:id', controller.remove);
+router.delete('/:id', eAdmin, controller.remove);
 
 module.exports = router;
